31bcb65b29d6bdb555076d70cc00455ac920f6f8
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-09-10 17:53:20 +0100

    Feat: Review modal done

---
+++ packages/nextjs/app/dashboard/_components/modal/modal.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import Star from "../../../assets/stars.png";
+
+type Props = {
+  isVisible: boolean;
+  setShowModal: (arg: boolean) => void;
+};
+
+const Modal = ({ isVisible, setShowModal }: Props) => {
+  if (!isVisible) return null;
+
+  const handleClose = (e: any) => {
+    if (e.target.id == "wrapper") {
+      return setShowModal(false);
+    }
+  };
+
+  return (
+    <div
+      onClick={handleClose}
+      id="wrapper"
+      className="fixed w-full inset-0 bg-black bg-opacity-25 backdrop-blur-sm z-50 flex justify-center items-center"
+    >
+      <div className="w-full flex flex-col">
+        <button
+          onClick={() => setShowModal(false)}
+          className="text-white absolute right-14 top-10 font-bold place-self-end"
+        >
+          X
+        </button>
+        <div className="w-full flex justify-center items-center">
+          <div className="bg-white w-[747px] flex justify-center items-center">
+            <div className="max-w-[451px] py-14 text-center flex flex-col justify-center items-center">
+              <h3 className="text-[24px] font-semibold text-center">Share your experience</h3>
+              <p>How would you rate this talent</p>
+              <Image src={Star} alt="img" />
+              <textarea placeholder="Add comment" className="mt-3 w-full border p-3"></textarea>
+
+              <div
+                onClick={() => setShowModal(false)}
+                className="w-full h-[24px] lg:h-[48px] mt-5 px-6 py-3  bg-[#2F66F6] font-medium text-[14px] lg:text-[16px] text-nowrap text-[#ffffff] text-center cursor-pointer"
+              >
+                Submit review
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- packages/nextjs/app/dashboard/page.tsx
+++ packages/nextjs/app/dashboard/page.tsx
@@ -5,12 +5,14 @@ import Image from "next/image";
 import Link from "next/link";
 import Avatar1 from "../assets/Avartar1.png";
 import Star from "../assets/stars.png";
+import Modal from "./_components/modal/modal";
 import RightModal from "./_components/right-modal";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./_components/ui/tabs";
 import type { NextPage } from "next";
 
 const Dashboard: NextPage = () => {
   const [showModal, setShowModal] = useState(false);
+  const [showRevModal, setShowRevModal] = useState(false);
 
   const reviews = [
     {
@@ -39,16 +41,18 @@ const Dashboard: NextPage = () => {
         <Image src={Avatar1} alt="image" />
         <h4 className="text-[18px] font-semibold">Kovács Lajos</h4>
         <Link href="/project-contract">
-          <div className="w-full h-[43px] lg:h-[48px] px-6 py-3 gap-2 rounded-[4px] bg-[#2F66F6] font-montserrat font-medium text-[14px] lg:text-[16px] leading-6 text-nowrap text-[#ffffff] text-center">
+          <div className="w-full cursor-pointer h-[43px] lg:h-[48px] px-6 py-3 gap-2 rounded-[4px] bg-[#2F66F6] font-montserrat font-medium text-[14px] lg:text-[16px] leading-6 text-nowrap text-[#ffffff] text-center">
             Hire
           </div>
         </Link>
-
-        <Link href="/">
-          <div className="w-full h-[43px] lg:h-[48px] px-6 py-3 gap-2 rounded-[4px] bg-[#696F8C] font-montserrat font-medium text-[14px] lg:text-[16px] leading-6 text-nowrap text-[#ffffff] text-center">
+        <div>
+          <div
+            onClick={() => setShowRevModal(true)}
+            className="w-full h-[43px] cursor-pointer lg:h-[48px] px-6 py-3 gap-2 rounded-[4px] bg-[#696F8C] font-montserrat font-medium text-[14px] lg:text-[16px] leading-6 text-nowrap text-[#ffffff] text-center"
+          >
             Leave a review
           </div>
-        </Link>
+        </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-3 mt-16">
         <div className="bg-white border-[#D7D9E4] border rounded-lg shadow-md p-6">
@@ -140,6 +144,7 @@ const Dashboard: NextPage = () => {
       </Tabs>
 
       <RightModal isVisible={showModal} setShowModal={setShowModal} />
+      <Modal isVisible={showRevModal} setShowModal={setShowRevModal} />
     </div>
   );
 };
